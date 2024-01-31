0f31be593dbe88d55952a6387faaefbe4ea813cc
Paiva8859 <gabrilaguilarpaiva@gmail.com>
2024-01-31 16:31:58 -0300

    tutorial

---
+++ StringsPython.py
@@ -0,0 +1,10 @@
+# Concatenando strings
+primeiro_nome = "Gabriel"
+ultimo_nome = "Paiva"
+nome_completo = primeiro_nome + " " + ultimo_nome
+
+# Imprimindo o nome completo
+print("Nome completo:", nome_completo)
+
+# Transformando para letras maiúsculas
+print("Nome em maiúsculas:", nome_completo.upper())
